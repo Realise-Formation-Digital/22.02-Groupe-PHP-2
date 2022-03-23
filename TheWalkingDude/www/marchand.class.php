0bfd38285f8ea53df8a0dd4cacf0312e0c9a2c98
GABSVN <j.gabriel.fds.1@gmail.com>
2022-03-23 17:14:08 +0100

    1st commit
    début classes

---
+++ TheWalkingDude/www/marchand.class.php
@@ -0,0 +1,60 @@
+<?php
+
+class Marchand{
+
+        protected $strength = rand(1,50);
+        protected exp = rand(1,50);
+        protected  $valeur1;
+        
+        
+        
+        
+        
+        // public fonction add() {
+        //     return $this->_valeur1 + $this->_valeur2;
+        // }
+        
+        // public fonction sub() {
+        //     return $this->_valeur1 - $this->_valeur2;
+        // }
+        
+        // public fonction mult() {
+        //     return $this->_valeur1 * $this->_valeur2;
+        // }
+        
+        
+        
+        // public fonction div() {
+        //     return $this->_valeur1 / $this->_valeur2;
+        // }
+        
+        
+        
+        // public function calculer($val1, $val2, $op) {
+        //     $this->_valeur1 = $val1;
+        //     $this->_valeur2 = $val2;
+        //     return $this->{$op}();
+            
+        // }
+        
+        
+        
+        
+        
+      
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+?>
