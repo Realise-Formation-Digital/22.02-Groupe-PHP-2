344eb48570e786d9d55185628683d381e0d5888d
Elyes1290 <elyes.naitliman@gmail.com>
2022-03-23 17:13:58 +0100

    Création de toutes les classes
    création class : héro, humain, monstre, marchand, armes

---
+++ www/Héro.class.php
@@ -0,0 +1,66 @@
+<?php
+
+class Humain {
+
+
+
+}    
+
+
+class Hero {
+
+        protected $life = 10;
+        protected $xp = 0;
+        protected $strength = 1;
+        protected $stamina = 1;
+        protected $bag = [];
+        protected $coins = 20;
+
+
+
+        protected function _combattre(){
+
+
+        }
+
+        private function _fuire(){
+
+
+        }
+
+        private function _mourir(){
+
+
+        }
+
+        private function _equiper(){
+
+
+        }
+
+        private function _desequiper(){
+
+
+        }
+
+        private function _acheter(){
+
+
+        }
+
+        private function _vendreDesequiper(){
+
+
+        }
+
+        private function _echanger(){
+
+
+        }
+
+        protected function _gagner(){
+
+
+        }
+
+    }
