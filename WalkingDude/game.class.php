dd1b38767f0863f154d6a62e91f321397a17bc0b
pvavrina <patrick@club-linux.ch>
2022-03-28 15:02:34 +0200

    updated file
    UPDATED

--- WalkingDude/game.class.php
+++ WalkingDude/game.class.php
@@ -3,54 +3,87 @@
 
 // Properties
 
-class Game {
-private $_hero;
-private $_monster;
-private $_merchant;
-private $_arms=[];
+class Game
+{
+    private $_hero;
+    private $_monster;
+    private $_merchant;
+    private $_weapon = [];
 
 
 // Methods
 
-function __construct() {
-    $this->_hero=new Hero();
-    $this->_monster=new Monster();
-    $this->_merchant=new Merchant();
-    $this->_arms=new Arms();
-}
+    function __construct()
+    {
+        $this->_hero = new Hero;
+        $this->_monster = new Monster;
+        $this->_merchant = new Merchant;
+        $this->_arms = new Weapon;
+    }
 
 
-public function setHero($_hero)
-{
-$this->_hero=$_hero;
-}
+    public function setHero($_hero)
+    {
+        $this->_hero = $_hero;
+    }
 
-public function getHero() {
-    return $this->_hero;
-}
+    public function getHero()
+    {
+        return $this->_hero;
+    }
 
-public function setMonster($_monster) {
-$this->_monster=$_monster;
-}
+    public function setMonster($_monster)
+    {
+        $this->_monster = $_monster;
+    }
 
-public function getMonster() {
-    return $this->_monster;
-}
+    public function getMonster()
+    {
+        return $this->_monster;
+    }
 
-public function setMerchant($_merchant) {
-$this->_merchant=$_merchant;
-}
+    public function setMerchant($_merchant)
+    {
+        $this->_merchant = $_merchant;
+    }
 
-public function getMerchant() {
-    return $this->_merchant;
-}
+    public function getMerchant()
+    {
+        return $this->_merchant;
+    }
 
-public function setArms($_arms) {
-$this->_arms=$_arms;
-}
+    public function setWeapon($_weapon)
+    {
+        $this->_weapon = $_weapon;
+    }
 
-public function getArms() {
-    return $this->_arms;
-}
+    public function getWeapon()
+    {
+        return $this->_weapon;
+    }
 
+
+
+    function attack()
+    {
+        return;
+    }
+
+
+    function defend()
+    {
+        return;
+    }
+
+
+    function kill()
+    {
+        return;
+    }
+
+
+    function live()
+    {
+        return;
+    }
 }
