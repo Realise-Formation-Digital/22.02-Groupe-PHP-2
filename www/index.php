0cfdeaa915dd468b7e03ffe7ce4dcd7451b3c694
Y0NAT <ezechiel.yonathan.favre@gmail.com>
2022-03-29 16:19:13 +0200

    MON VRAI KOMIT :)
    sas

--- www/index.php
+++ www/index.php
@@ -123,22 +123,6 @@
 
 
 
-
-
-
-<!-- <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="staticBackdropLabel">The Walking Dudes</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-<div class="modal-body">
-    <a href="#" role="button" class="btn btn-secondary popover-test" title="Popover title" data-bs-content="Popover body content is set in this attribute.">button</a>
-  <hr>
-    
-</div> -->
-
 <!-- <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
