4e10fbd9424279a3255bce77ea65fabe2b7384e4
GABSVN <j.gabriel.fds.1@gmail.com>
2022-03-29 10:59:54 +0200

    4th
    still in process

---
+++ WalkingDude/fight.php
@@ -0,0 +1,28 @@
+<?php
+
+// Hero or Monster attacks first?
+
+random 
+
+
+// Hero attacks first
+
+if Hero's power > Monster's stamina -> Hero strikes Monster
+else Hero is hurt 
+
+
+// Monster attacks
+
+if Monster's power > Hero's stamina -> Monster strikes Hero
+else Monster is hurt
+
+
+// who is alive?
+
+if Hero's life > 0 -> the fight continues 
+else Hero is dead -> end of the game
+
+
+// Hero's and Monster's stats 
+
+?>
