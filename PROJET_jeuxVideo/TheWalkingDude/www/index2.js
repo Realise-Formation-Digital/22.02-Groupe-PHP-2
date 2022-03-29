22e38740da09a16b58c04e532ed0a4eefcb01950
GABSVN <j.gabriel.fds.1@gmail.com>
2022-03-29 10:59:54 +0200

    4th
    still in process

--- PROJET_jeuxVideo/TheWalkingDude/www/index2.js
+++
@@ -1,8 +0,0 @@
-var launch = document.getElementById('choix');
-launch.addEventListener('click', mafonction);
- 
-function mafonction(){
-    var lancer = Math.floor(Math.random()*6 +1); // choix d'un chiffre aléatoire entre 1 et 6
-    document.getElementById('chiffre').innerHTML = lancer; //affiche le chiffre
-    document.getElementById('choiximg').src = "img/" + lancer;
-}
