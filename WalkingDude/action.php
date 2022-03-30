b2c2aa712b7ef1f0c9cc2baff684b796dcd39d6f
Elyes1290 <elyes.naitliman@gmail.com>
2022-03-30 13:24:35 +0200

    ma version remis à jours

--- WalkingDude/action.php
+++
@@ -1,8 +0,0 @@
-Hello <?php echo htmlspecialchars($_POST['name']); ?>.
-
-<br>
-
-<div>
-<a class="btn btn-primary" role="button">
-<button class="btn btn-primary" type="submit">Continue</button>
-</div>
